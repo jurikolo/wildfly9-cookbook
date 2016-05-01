61306f18a87673035ec46189dd5dbe178a197bbf
Jurijs Kolomijecs <jurikolo@yandex.com>
2016-05-01 16:27:38 +0300

    Added standalone clustered mode

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# wildfly9-domain-example
-This is example of how to setup Wildfly 9 domain with 2 slaves as described in
+# Wildfly 9 domain mode no cluster
+This example configures Wildfly 9 domain with 2 slaves as described in a
 book "Wildfly Cookbook", pages 83-93
 
 HOW TO
@@ -7,6 +7,27 @@ HOW TO
 1. Copy directories master, slave-1 and slave-2 to the Wildfly9 directory
 2. Run domain controller: ./bin/domain.sh -Djboss.domain.base.dir=master
 3. Run slave-1:
-./bin/domain.sh -Djboss.domain.base.dir=slave-1 -Djboss.domain.master.address=127.0.0.1
+./bin/domain.sh -Djboss.domain.base.dir=slave-1 \
+  -Djboss.domain.master.address=127.0.0.1
 4. Run slave-2:
-./bin/domain.sh -Djboss.domain.base.dir=slave-2 -Djboss.domain.master.address=127.0.0.1
+./bin/domain.sh -Djboss.domain.base.dir=slave-2 \
+  -Djboss.domain.master.address=127.0.0.1
+
+# Wildfly 9 standalone mode clustered
+This example configures 2 standalone Wildfly 9 instances in a cluster as
+described in a book "Wildfly Cookbook", pages 176-185
+HOW TO
+------
+1. Copy directories cl-std-node-1 and cl-std-node-2 to the Wildfly9 directory
+2. Run node-1:
+./bin/standalone.sh -Djboss.server.base.dir=cl-std-node-1 \
+  --server-config=standalone-ha.xml \
+  -Djboss.socket.binding.port-offset=100 \
+  -Djboss.node.name=node-1
+3. Run node-2:
+./bin/standalone.sh -Djboss.server.base.dir=cl-std-node-2 \
+  --server-config=standalone-ha.xml \
+  -Djboss.socket.binding.port-offset=200 \
+  -Djboss.node.name=node-2
+4. Visit http://localhost:8180/cluster-test/ and
+http://localhost:8280/cluster-test/ for results
---
+++ cl-std-node-2/tmp/cluster-test.war/org/apache/jsp/index_jsp.java
@@ -0,0 +1,89 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.tomcat.InstanceManager _jsp_instancemanager;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_instancemanager = org.apache.jasper.runtime.InstanceManagerFactory.getInstanceManager(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html");
+      response.addHeader("X-Powered-By", "JSP/2.2");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("<html>\n");
+      out.write("<body>\n");
+      out.write("<h2>WildFly::Cookbook::Cluster Test</h2>\n");
+      out.write("\n");
+
+    Object sessAttr = session.getAttribute("visitors");
+    int visitors = sessAttr == null ? 0 : Integer.valueOf(sessAttr+"");
+    System.out.println("********************************+");
+    System.out.println("Visitor(s): " + visitors);
+    System.out.println("********************************+");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("<h2>Visitor(s): ");
+      out.print(visitors);
+      out.write("</h2>\n");
+      out.write("\n");
+
+    session.setAttribute("visitors", ++visitors);
+
+      out.write("\n");
+      out.write("\n");
+      out.write("</body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
